033a722ee1ab59a476c8beaaed65a3259faf4182
Sebastian Matysik <smatysik@nvidia.com>
2023-04-21 21:52:44 +0200

    Stop using parser state in nvbmp decoder

--- extensions/nvbmp/decoder.cpp
+++ extensions/nvbmp/decoder.cpp
@@ -82,22 +82,24 @@ static nvimgcdcsStatus_t nvbmp_decoder_decode(nvimgcdcsDecoder_t decoder, nvimgc
     size_t output_size = 0;
     nvimgcdcsIoStreamDesc_t io_stream = code_stream->io_stream;
     io_stream->size(io_stream->instance, &size);
-    code_stream->parse_state->buffer.resize(size);
+    std::vector<unsigned char> buffer(size);
+    static constexpr int kHeaderStart = 14;
+    io_stream->seek(io_stream->instance, kHeaderStart, SEEK_SET);
+    uint32_t header_size;
+    io_stream->read(io_stream->instance, &output_size, &header_size, sizeof(header_size));
     io_stream->seek(io_stream->instance, 0, SEEK_SET);
-    io_stream->read(io_stream->instance, &output_size, &code_stream->parse_state->buffer[0], size);
+    io_stream->read(io_stream->instance, &output_size, &buffer[0], size);
     if (output_size != size) {
         return NVIMGCDCS_STATUS_BAD_CODESTREAM;
     }
 
-    static constexpr int kHeaderStart = 14;
     unsigned char* host_buffer = reinterpret_cast<unsigned char*>(image_info.buffer);
     for (size_t p = 0; p < image_info.num_planes; p++) {
         for (size_t y = 0; y < image_info.plane_info[p].height; y++) {
             for (size_t x = 0; x < image_info.plane_info[p].width; x++) {
                 host_buffer[(image_info.num_planes - p - 1) * image_info.plane_info[p].height * image_info.plane_info[p].width +
                             (image_info.plane_info[p].height - y - 1) * image_info.plane_info[p].width + x] =
-                    code_stream->parse_state->buffer[kHeaderStart + code_stream->parse_state->header_size +
-                                                     image_info.num_planes * (y * image_info.plane_info[p].width + x) + p];
+                    buffer[kHeaderStart + header_size + image_info.num_planes * (y * image_info.plane_info[p].width + x) + p];
             }
         }
     }
