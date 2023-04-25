c16a03472dc287352350d3f8f2f064a36eb50661
Sebastian Matysik <smatysik@nvidia.com>
2023-04-25 09:53:24 +0200

    IMGCODECS-241 Added format checks for pnm encoder

--- extensions/nvpnm/encoder.cpp
+++ extensions/nvpnm/encoder.cpp
@@ -51,22 +51,41 @@ static nvimgcdcsStatus_t pnm_can_encode(nvimgcdcsEncoder_t encoder, nvimgcdcsPro
         if (*result != NVIMGCDCS_PROCESSING_STATUS_SUCCESS) {
             continue;
         }
-        if ((*image) != nullptr) {
-            nvimgcdcsImageInfo_t image_info{NVIMGCDCS_STRUCTURE_TYPE_IMAGE_INFO, 0};
-            (*image)->getImageInfo((*image)->instance, &image_info);
+        if (params->mct_mode != NVIMGCDCS_MCT_MODE_RGB) {
+            *result |= NVIMGCDCS_PROCESSING_STATUS_MCT_UNSUPPORTED;
+        }
 
-            //Assumed planar format
-            if ((image_info.num_planes > 4 || image_info.num_planes == 2)) {
-                NVIMGCDCS_E_LOG_INFO("cannot encode because not supported number of components");
-                *result = NVIMGCDCS_PROCESSING_STATUS_NUM_PLANES_UNSUPPORTED;
-                continue;
+        nvimgcdcsImageInfo_t image_info{NVIMGCDCS_STRUCTURE_TYPE_IMAGE_INFO, 0};
+        (*image)->getImageInfo((*image)->instance, &image_info);
+        nvimgcdcsImageInfo_t out_image_info{NVIMGCDCS_STRUCTURE_TYPE_IMAGE_INFO, 0};
+        (*code_stream)->getImageInfo((*code_stream)->instance, &out_image_info);
+
+        if (image_info.color_spec != NVIMGCDCS_COLORSPEC_SRGB) {
+            *result |= NVIMGCDCS_PROCESSING_STATUS_COLOR_SPEC_UNSUPPORTED;
+        }
+        if (image_info.chroma_subsampling != NVIMGCDCS_SAMPLING_NONE) {
+            *result |= NVIMGCDCS_PROCESSING_STATUS_SAMPLING_UNSUPPORTED;
+        }
+        if (out_image_info.chroma_subsampling != NVIMGCDCS_SAMPLING_NONE) {
+            *result |= NVIMGCDCS_PROCESSING_STATUS_SAMPLING_UNSUPPORTED;
+        }
+        if ((image_info.sample_format != NVIMGCDCS_SAMPLEFORMAT_P_RGB) && (image_info.sample_format != NVIMGCDCS_SAMPLEFORMAT_I_RGB)) {
+            *result |= NVIMGCDCS_PROCESSING_STATUS_SAMPLE_FORMAT_UNSUPPORTED;
+        }
+        if (((image_info.sample_format == NVIMGCDCS_SAMPLEFORMAT_P_RGB) && (image_info.num_planes != 3)) ||
+            ((image_info.sample_format == NVIMGCDCS_SAMPLEFORMAT_I_RGB) && (image_info.num_planes != 1))) {
+            *result |= NVIMGCDCS_PROCESSING_STATUS_NUM_PLANES_UNSUPPORTED;
+        }
+
+        for (uint32_t p = 0; p < image_info.num_planes; ++p) {
+            if (image_info.plane_info[p].sample_type != NVIMGCDCS_SAMPLE_DATA_TYPE_UINT8 &&
+                (image_info.plane_info[p].sample_type != NVIMGCDCS_SAMPLE_DATA_TYPE_UINT16)) {
+                *result |= NVIMGCDCS_PROCESSING_STATUS_SAMPLE_TYPE_UNSUPPORTED;
             }
 
-            if ((image_info.plane_info[0].sample_type != NVIMGCDCS_SAMPLE_DATA_TYPE_UINT8) &&
-                image_info.plane_info[0].sample_type != NVIMGCDCS_SAMPLE_DATA_TYPE_UINT16) {
-                NVIMGCDCS_E_LOG_INFO("cannot encode because not supported data type #" << image_info.plane_info[0].sample_type);
-                *result = NVIMGCDCS_PROCESSING_STATUS_SAMPLE_TYPE_UNSUPPORTED;
-                continue;
+            if (((image_info.sample_format == NVIMGCDCS_SAMPLEFORMAT_P_RGB) && (image_info.plane_info[p].num_channels != 1)) ||
+                ((image_info.sample_format == NVIMGCDCS_SAMPLEFORMAT_I_RGB) && (image_info.plane_info[p].num_channels != 3))) {
+                *result |= NVIMGCDCS_PROCESSING_STATUS_NUM_CHANNELS_UNSUPPORTED;
             }
         }
     }
@@ -197,15 +216,20 @@ static nvimgcdcsStatus_t pnm_encode(nvimgcdcsEncoder_t encoder, nvimgcdcsImageDe
         write_pnm<unsigned char, NVIMGCDCS_SAMPLEFORMAT_I_RGB>(code_stream->io_stream, host_buffer, image_info.plane_info[0].row_stride,
             NULL, 0, NULL, 0, NULL, 0, image_info.plane_info[0].width, image_info.plane_info[0].height,
             image_info.plane_info[0].num_channels, image_info.plane_info[0].sample_type == NVIMGCDCS_SAMPLE_DATA_TYPE_UINT8 ? 8 : 16);
-    } else {
+        image->imageReady(image->instance, NVIMGCDCS_PROCESSING_STATUS_SUCCESS);
+    } else if (NVIMGCDCS_SAMPLEFORMAT_P_RGB == image_info.sample_format) {
         write_pnm<unsigned char>(code_stream->io_stream, host_buffer, image_info.plane_info[0].row_stride,
             host_buffer + image_info.plane_info[0].row_stride * image_info.plane_info[0].height, image_info.plane_info[1].row_stride,
             host_buffer + +image_info.plane_info[0].row_stride * image_info.plane_info[0].height +
                 image_info.plane_info[1].row_stride * image_info.plane_info[0].height,
             image_info.plane_info[2].row_stride, NULL, 0, image_info.plane_info[0].width, image_info.plane_info[0].height,
             image_info.num_planes, image_info.plane_info[0].sample_type == NVIMGCDCS_SAMPLE_DATA_TYPE_UINT8 ? 8 : 16);
+        image->imageReady(image->instance, NVIMGCDCS_PROCESSING_STATUS_SUCCESS);
+    } else {
+        image->imageReady(
+            image->instance, NVIMGCDCS_PROCESSING_STATUS_SAMPLE_FORMAT_UNSUPPORTED | NVIMGCDCS_PROCESSING_STATUS_FAIL);
+
     }
-    image->imageReady(image->instance, NVIMGCDCS_PROCESSING_STATUS_SUCCESS);
     return NVIMGCDCS_STATUS_SUCCESS;
 }
 
