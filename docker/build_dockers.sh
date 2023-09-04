759de1af50d690cac1fd1368fb504b9ff4b29074
Joaquin Anton <janton@nvidia.com>
2023-09-04 19:28:14 +0200

    Downgrade to GCC 9 (devtoolset-9)
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- docker/build_dockers.sh
+++ docker/build_dockers.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -ex
 
 IMG_ARCH="x86_64"
-IMG_VERSION="1"
 IMG_UBUNTU="20.04"
 
 CUDA_VERSIONS=("11.8.0" "12.1.1")
@@ -12,9 +11,14 @@ for IMG_CUDA_VERSION in "${CUDA_VERSIONS[@]}"; do
     CUDA_IMAGE="cuda${IMG_CUDA_VERSION}.${IMG_ARCH}"
     DEPS_IMAGE="nvimgcodecs_deps.${IMG_ARCH}"
 
-    BUILDER_IMAGE="gitlab-master.nvidia.com:5005/cuda-hpc-libraries/nvimagecodec/build-linux-${IMG_ARCH}:cuda-${IMG_CUDA_VERSION}-v${IMG_VERSION}"
-    TEST_IMAGE="gitlab-master.nvidia.com:5005/cuda-hpc-libraries/nvimagecodec/runner-linux-${IMG_ARCH}:cuda-${IMG_CUDA_VERSION}-v${IMG_VERSION}"
-    DOCS_IMAGE="gitlab-master.nvidia.com:5005/cuda-hpc-libraries/nvimagecodec/docs-linux-${IMG_ARCH}:cuda-${IMG_CUDA_VERSION}-v${IMG_VERSION}"
+    BUILDER_IMAGE_VER="2"
+    BUILDER_IMAGE="gitlab-master.nvidia.com:5005/cuda-hpc-libraries/nvimagecodec/build-linux-${IMG_ARCH}:cuda-${IMG_CUDA_VERSION}-v${BUILDER_IMAGE_VER}"
+
+    TEST_IMAGE_VER="1"
+    TEST_IMAGE="gitlab-master.nvidia.com:5005/cuda-hpc-libraries/nvimagecodec/runner-linux-${IMG_ARCH}:cuda-${IMG_CUDA_VERSION}-v${TEST_IMAGE_VER}"
+
+    DOCS_IMAGE_VER="1"
+    DOCS_IMAGE="gitlab-master.nvidia.com:5005/cuda-hpc-libraries/nvimagecodec/docs-linux-${IMG_ARCH}:cuda-${IMG_CUDA_VERSION}-v${DOCS_IMAGE_VER}"
 
     # Building dependencies
     docker build -t ${CUDA_IMAGE} -f docker/Dockerfile.cuda${IMG_CUDA_SHORT_VER}.${IMG_ARCH}.deps docker
