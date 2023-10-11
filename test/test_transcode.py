b7404f08db24a739b4d4e0bdfda5421fe6968da1
Sebastian Matysik <smatysik@nvidia.com>
2023-10-11 13:17:17 +0200

    Replace jpeg images with different chroma subsampling with licensed files in tests

--- test/test_transcode.py
+++ test/test_transcode.py
@@ -34,29 +34,29 @@ def file_md5(file_name):
     ("base/4k_lossless.jp2", "jpeg", "4k_lossless-jp2-422.jpg","--chroma_subsampling 422", "288ea71d08e74876d90c89c33284c56f"),
     
     #jpeg various input chroma subsampling
-    ("base/rgb_25_410.jpg", "bmp",    "rgb_25_410-jpg.bmp", "", "09a9dbb7b75278197c58e7ddcb93c4cb"),
-    ("base/rgb_25_410.jpg", "jpeg",   "rgb_25_410-jpg.jpg", "", "d2b17165ebdc89d84d8db39d71524bf5"),
-    ("base/rgb_25_410.jpg", "jpeg2k", "rgb_25_410-jpg.jp2", "--enc_color_trans true", "6562455982c8d6a1ee6484cb8c635cb0"),
+    ("jpeg/padlock-406986_640_410.jpg", "bmp",    "padlock-406986_640_410-jpg.bmp", "", "01d3488f82c6422a76db5ab0e89e8a86"),
+    ("jpeg/padlock-406986_640_410.jpg", "jpeg",   "padlock-406986_640_410-jpg.jpg", "", "31f72adf6f8553118927df00a1a8adc3"),
+    ("jpeg/padlock-406986_640_410.jpg", "jpeg2k", "padlock-406986_640_410-jpg.jp2", "--enc_color_trans true", "06d36693b45d45214ea672a0461b9540"),
     
-    ("base/rgb_25_411.jpg", "bmp",    "rgb_25_411-jpg.bmp", "", "e9e7261b325d3f2895f0281147f61e0b"),
-    ("base/rgb_25_411.jpg", "jpeg",   "rgb_25_411-jpg.jpg", "", "46cc411a9770d1e19e3015cf97c97db5"),
-    ("base/rgb_25_411.jpg", "jpeg2k", "rgb_25_411-jpg.jp2", "--enc_color_trans true", "f16e98162f7cd444abae48452cfd8f87"),
+    ("jpeg/padlock-406986_640_411.jpg", "bmp",    "padlock-406986_640_411-jpg.bmp", "", "925c8465bbd6059570350612abc47ecc"),
+    ("jpeg/padlock-406986_640_411.jpg", "jpeg",   "padlock-406986_640_411-jpg.jpg", "", "31fcb68b4641c6db3e0464d06dffbbda"),
+    ("jpeg/padlock-406986_640_411.jpg", "jpeg2k", "padlock-406986_640_411-jpg.jp2", "--enc_color_trans true", "08c7921bf34adb4ea180fa1359fe4cca"),
     
-    ("base/rgb_25_420.jpg", "bmp",    "rgb_25_420-jpg.bmp", "", "4be980aed1726182b873a55611574303"),
-    ("base/rgb_25_420.jpg", "jpeg",   "rgb_25_420-jpg.jpg", "", "35893dc324cd29042c13aa8d9ac3374d"),
-    ("base/rgb_25_420.jpg", "jpeg2k", "rgb_25_420-jpg.jp2", "--enc_color_trans true", "4e7a8881fe9c4ebaddf8855a43794080"),
+    ("jpeg/padlock-406986_640_420.jpg", "bmp",    "padlock-406986_640_420-jpg.bmp", "", "a04595f50cfbe8d9d5726a8eb3dc70f1"),
+    ("jpeg/padlock-406986_640_420.jpg", "jpeg",   "padlock-406986_640_420-jpg.jpg", "", "740d7a4ea7237793033a1de88f77e610"),
+    ("jpeg/padlock-406986_640_420.jpg", "jpeg2k", "padlock-406986_640_420-jpg.jp2", "--enc_color_trans true", "10d7bf042a3fc42c90c762eebce16f09"),
 
-    ("base/rgb_25_422.jpg", "bmp",    "rgb_25_422-jpg.bmp", "", "9ab1754b7c192e03090863285dba68ed"),
-    ("base/rgb_25_422.jpg", "jpeg",   "rgb_25_422-jpg.jpg", "", "7b71fa9f5359840e004ebee7921fe554"),
-    ("base/rgb_25_422.jpg", "jpeg2k", "rgb_25_422-jpg.jp2", "--enc_color_trans true", "80aad59a2e0e1025afeea235daa34559"),
+    ("jpeg/padlock-406986_640_422.jpg", "bmp",    "padlock-406986_640_422-jpg.bmp", "", "fca403b759d1ef4173e35c134258719a"),
+    ("jpeg/padlock-406986_640_422.jpg", "jpeg",   "padlock-406986_640_422-jpg.jpg", "", "b225ca4dcc19412d9c18caf524f9080f"),
+    ("jpeg/padlock-406986_640_422.jpg", "jpeg2k", "padlock-406986_640_422-jpg.jp2", "--enc_color_trans true", "a9e445c5df6ef56f1f7f695bae42b807"),
 
-    ("base/rgb_25_440.jpg", "bmp",    "rgb_25_440-jpg.bmp", "", "ba863e3f78be2238300cf2ef5c2d60e9"),
-    ("base/rgb_25_440.jpg", "jpeg",   "rgb_25_440-jpg.jpg", "", "57249544fdace822002cbbfaa5988345"),
-    ("base/rgb_25_440.jpg", "jpeg2k", "rgb_25_440-jpg.jp2", "--enc_color_trans true", "6c7b06b6bcc03360e05efe2ad8995d66"),
+    ("jpeg/padlock-406986_640_440.jpg", "bmp",    "padlock-406986_640_440-jpg.bmp", "", "aa26bef4b98a8c56f837426bd33f2eaf"),
+    ("jpeg/padlock-406986_640_440.jpg", "jpeg",   "padlock-406986_640_440-jpg.jpg", "", "bd29ce0b525a33e8919362c96aee7aa6"),
+    ("jpeg/padlock-406986_640_440.jpg", "jpeg2k", "padlock-406986_640_440-jpg.jp2", "--enc_color_trans true", "a90b221752545b5f4640494e9d67ff6c"),
 
-    ("base/rgb_25_444.jpg", "bmp",    "rgb_25_444-jpg.bmp", "", "49f3b3f8ef8c792520bd43436ff76153"),
-    ("base/rgb_25_444.jpg", "jpeg",   "rgb_25_444-jpg.jpg", "", "9f9a2c181e981be66d7f0f2a0139935e"),
-    ("base/rgb_25_444.jpg", "jpeg2k", "rgb_25_444-jpg.jp2", "--enc_color_trans true", "dda34d8944767382cc532cf334b9abbb"),
+    ("jpeg/padlock-406986_640_444.jpg", "bmp",    "padlock-406986_640_444-jpg.bmp", "", "0b5846e30997034867d08843e527b951"),
+    ("jpeg/padlock-406986_640_444.jpg", "jpeg",   "padlock-406986_640_444-jpg.jpg", "", "8d7e0c2b8a458f6ec4b6af03cc1c9f16"),
+    ("jpeg/padlock-406986_640_444.jpg", "jpeg2k", "padlock-406986_640_444-jpg.jp2", "--enc_color_trans true", "7c6a2375a8b1387036bd7a02c19e8f8e"),
 
     #test pnm
     ("base/4k_lossless.bmp", "pnm", "4k_lossless-bmp.ppm","", "0a26f98671a4eba8e2092d2777fc701d"),
