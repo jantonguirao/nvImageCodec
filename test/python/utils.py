3a635b55ac3e3ca9665e895ca33bcef88c08cfd4
Joaquin Anton <janton@nvidia.com>
2023-10-24 09:14:52 +0200

    Skip jpeg lossless when not supported
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

---
+++ test/python/utils.py
@@ -0,0 +1,38 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+def get_nvjpeg_ver():
+    import ctypes as c
+    nvjpeg_libname = 'libnvjpeg.so'
+    nvjpeg_lib = c.CDLL(nvjpeg_libname)
+    nvjpeg_ver_major, nvjpeg_ver_minor, nvjpeg_ver_patch = (c.c_int(), c.c_int(), c.c_int())
+    nvjpeg_lib.nvjpegGetProperty(0, c.byref(nvjpeg_ver_major))
+    nvjpeg_lib.nvjpegGetProperty(1, c.byref(nvjpeg_ver_minor))
+    nvjpeg_lib.nvjpegGetProperty(2, c.byref(nvjpeg_ver_patch))
+    return nvjpeg_ver_major.value, nvjpeg_ver_minor.value, nvjpeg_ver_patch.value
+
+def get_cuda_compute_capability(device_id=0):
+    compute_cap = 0
+    try:
+        import pynvml
+        pynvml.nvmlInit()
+        handle = pynvml.nvmlDeviceGetHandleByIndex(device_id)
+        compute_cap = pynvml.nvmlDeviceGetCudaComputeCapability(handle)
+        compute_cap = compute_cap[0] + compute_cap[1] / 10.
+    except ModuleNotFoundError:
+        print("NVML not found")
+    return compute_cap
