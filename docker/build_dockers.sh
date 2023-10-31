cd4e04ce08ed4861d534131e747bbb5b5c4b56c9
Joaquin Anton <janton@nvidia.com>
2023-10-31 09:02:52 +0100

    Add latest and versioned dockers
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- docker/build_dockers.sh
+++ docker/build_dockers.sh
@@ -13,11 +13,11 @@ docker buildx inspect --bootstrap
 ####### BASE IMAGES #######
 
 # Manylinux2014 with GCC 9
-export MANYLINUX_GCC9="${REGISTRY_PREFIX}manylinux2014_${ARCH}.gcc9:v${VERSION}"
+export MANYLINUX_GCC9="${REGISTRY_PREFIX}manylinux2014_${ARCH}.gcc9"
 docker buildx build \
     --cache-to type=inline \
     --cache-from type=registry,ref=${MANYLINUX_GCC9} \
-    -t ${MANYLINUX_GCC9} \
+    -t ${MANYLINUX_GCC9} -t ${MANYLINUX_GCC9}:v${VERSION} \
     -f docker/Dockerfile.gcc9 \
     --build-arg "FROM_IMAGE_NAME=quay.io/pypa/manylinux2014_${ARCH}" \
     --platform ${PLATFORM} \
@@ -25,11 +25,11 @@ docker buildx build \
     docker
 
 # Manylinux2014 with GCC 10
-export MANYLINUX_GCC10="${REGISTRY_PREFIX}manylinux2014_${ARCH}.gcc10:v${VERSION}"
+export MANYLINUX_GCC10="${REGISTRY_PREFIX}manylinux2014_${ARCH}.gcc10"
 docker buildx build \
     --cache-to type=inline \
     --cache-from type=registry,ref=${MANYLINUX_GCC10} \
-    -t ${MANYLINUX_GCC10} \
+    -t ${MANYLINUX_GCC10} -t ${MANYLINUX_GCC10}:v${VERSION} \
     -f docker/Dockerfile.gcc10 \
     --build-arg "FROM_IMAGE_NAME=quay.io/pypa/manylinux2014_${ARCH}" \
     --platform ${PLATFORM} \
@@ -37,33 +37,33 @@ docker buildx build \
     docker
 
 # CUDA 11.8.0
-export CUDA_118="${REGISTRY_PREFIX}cuda11.8-${ARCH}:v${VERSION}"
+export CUDA_118="${REGISTRY_PREFIX}cuda11.8-${ARCH}"
 docker buildx build \
     --cache-to type=inline \
     --cache-from type=registry,ref=${CUDA_118} \
-    -t ${CUDA_118} \
+    -t ${CUDA_118} -t ${CUDA_118}:v${VERSION} \
     -f docker/Dockerfile.cuda118.${ARCH}.deps \
     --platform ${PLATFORM} \
     --push \
     docker
 
 # CUDA 12.3.0
-export CUDA_123="${REGISTRY_PREFIX}cuda12.3-${ARCH}:v${VERSION}"
+export CUDA_123="${REGISTRY_PREFIX}cuda12.3-${ARCH}"
 docker buildx build \
     --cache-to type=inline \
     --cache-from type=registry,ref=${CUDA_123} \
-    -t ${CUDA_123} \
+    -t ${CUDA_123} -t ${CUDA_123}:v${VERSION} \
     -f docker/Dockerfile.cuda123.${ARCH}.deps \
     --platform ${PLATFORM} \
     --push \
     docker
 
 # GCC 9 (minimum supported)
-export DEPS_GCC9="${REGISTRY_PREFIX}nvimgcodec_deps-${ARCH}-gcc9:v${VERSION}"
+export DEPS_GCC9="${REGISTRY_PREFIX}nvimgcodec_deps-${ARCH}-gcc9"
 docker buildx build \
     --cache-to type=inline \
     --cache-from type=registry,ref=${DEPS_GCC9} \
-    -t ${DEPS_GCC9} \
+    -t ${DEPS_GCC9} -t ${DEPS_GCC9}:v${VERSION} \
     -f docker/Dockerfile.deps \
     --build-arg "FROM_IMAGE_NAME=${MANYLINUX_GCC9}" \
     --build-arg "ARCH=${ARCH}" \
@@ -72,11 +72,11 @@ docker buildx build \
     docker
 
 # GCC 10
-export DEPS_GCC10="${REGISTRY_PREFIX}nvimgcodec_deps-${ARCH}:v${VERSION}"
+export DEPS_GCC10="${REGISTRY_PREFIX}nvimgcodec_deps-${ARCH}"
 docker buildx build \
     --cache-to type=inline \
     --cache-from type=registry,ref=${DEPS_GCC10} \
-    -t ${DEPS_GCC10} \
+    -t ${DEPS_GCC10} -t ${DEPS_GCC10}:v${VERSION} \
     -f docker/Dockerfile.deps \
     --build-arg "FROM_IMAGE_NAME=${MANYLINUX_GCC10}" \
     --build-arg "ARCH=${ARCH}" \
@@ -87,11 +87,11 @@ docker buildx build \
 ####### BUILDER IMAGES #######
 
 # GCC 9 (minimum supported), CUDA 11.8
-export BUILDER_GCC9_CUDA_118="${REGISTRY_PREFIX}builder-cuda-11.8-gcc9-${ARCH}:v${VERSION}"
+export BUILDER_GCC9_CUDA_118="${REGISTRY_PREFIX}builder-cuda-11.8-gcc9-${ARCH}"
 docker buildx build \
     --cache-to type=inline \
     --cache-from type=registry,ref=${BUILDER_GCC9_CUDA_118} \
-    -t ${BUILDER_GCC9_CUDA_118} \
+    -t ${BUILDER_GCC9_CUDA_118} -t ${BUILDER_GCC9_CUDA_118}:v${VERSION} \
     -f docker/Dockerfile.cuda.deps \
     --build-arg "FROM_IMAGE_NAME=${DEPS_GCC9}" \
     --build-arg "CUDA_IMAGE=${CUDA_118}" \
@@ -100,11 +100,11 @@ docker buildx build \
     docker
 
 # GCC 10, CUDA 11.8
-export BUILDER_CUDA_118="${REGISTRY_PREFIX}builder-cuda-11.8-${ARCH}:v${VERSION}"
+export BUILDER_CUDA_118="${REGISTRY_PREFIX}builder-cuda-11.8-${ARCH}"
 docker buildx build \
     --cache-to type=inline \
     --cache-from type=registry,ref=${BUILDER_CUDA_118} \
-    -t ${BUILDER_CUDA_118} \
+    -t ${BUILDER_CUDA_118} -t ${BUILDER_CUDA_118}:v${VERSION} \
     -f docker/Dockerfile.cuda.deps \
     --build-arg "FROM_IMAGE_NAME=${DEPS_GCC10}" \
     --build-arg "CUDA_IMAGE=${CUDA_118}" \
@@ -113,11 +113,11 @@ docker buildx build \
     docker
 
 # GCC 10, CUDA 12.3
-export BUILDER_CUDA_123="${REGISTRY_PREFIX}builder-cuda-12.3-${ARCH}:v${VERSION}"
+export BUILDER_CUDA_123="${REGISTRY_PREFIX}builder-cuda-12.3-${ARCH}"
 docker buildx build \
     --cache-to type=inline \
     --cache-from type=registry,ref=${BUILDER_CUDA_123} \
-    -t ${BUILDER_CUDA_123} \
+    -t ${BUILDER_CUDA_123} -t ${BUILDER_CUDA_123}:v${VERSION} \
     -f docker/Dockerfile.cuda.deps \
     --build-arg "FROM_IMAGE_NAME=${DEPS_GCC10}" \
     --build-arg "CUDA_IMAGE=${CUDA_123}" \
@@ -128,11 +128,11 @@ docker buildx build \
 ####### TEST IMAGES #######
 
 # CUDA 11.3 (minimum supported)
-export RUNNER_CUDA_113="${REGISTRY_PREFIX}runner-cuda-11.3-${ARCH}:v${VERSION}"
+export RUNNER_CUDA_113="${REGISTRY_PREFIX}runner-cuda-11.3-${ARCH}"
 docker buildx build \
     --cache-to type=inline \
     --cache-from type=registry,ref=${RUNNER_CUDA_113} \
-    -t ${RUNNER_CUDA_113} \
+    -t ${RUNNER_CUDA_113} -t ${RUNNER_CUDA_113}:v${VERSION} \
     -f docker/Dockerfile.${ARCH} \
     --build-arg "BASE=nvidia/cuda:11.3.1-runtime-ubuntu20.04" \
     --build-arg "VER_CUDA=11.3.1" \
@@ -142,11 +142,11 @@ docker buildx build \
     docker
 
 # CUDA 11.8
-export RUNNER_CUDA_118="${REGISTRY_PREFIX}runner-cuda-11.8-${ARCH}:v${VERSION}"
+export RUNNER_CUDA_118="${REGISTRY_PREFIX}runner-cuda-11.8-${ARCH}"
 docker buildx build \
     --cache-to type=inline \
     --cache-from type=registry,ref=${RUNNER_CUDA_118} \
-    -t ${RUNNER_CUDA_118} \
+    -t ${RUNNER_CUDA_118} -t ${RUNNER_CUDA_118}:v${VERSION} \
     -f docker/Dockerfile.${ARCH} \
     --build-arg "BASE=nvidia/cuda:11.8.0-runtime-ubuntu20.04" \
     --build-arg "VER_CUDA=11.8.0" \
@@ -156,11 +156,11 @@ docker buildx build \
     docker
 
 # CUDA 12.1
-export RUNNER_CUDA_121="${REGISTRY_PREFIX}runner-cuda-12.1-${ARCH}:v${VERSION}"
+export RUNNER_CUDA_121="${REGISTRY_PREFIX}runner-cuda-12.1-${ARCH}"
 docker buildx build \
     --cache-to type=inline \
     --cache-from type=registry,ref=${RUNNER_CUDA_121} \
-    -t ${RUNNER_CUDA_121} \
+    -t ${RUNNER_CUDA_121} -t ${RUNNER_CUDA_121}:v${VERSION} \
     -f docker/Dockerfile.${ARCH} \
     --build-arg "BASE=nvidia/cuda:12.1.1-runtime-ubuntu20.04" \
     --build-arg "VER_CUDA=12.1.1" \
